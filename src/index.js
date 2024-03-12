562e35fd502d0bbacaccfabc22341003ee4d7da1
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-03-12 19:19:07 +0200

    importing BrowserRouter as Router and QueryClientProvider and passing it to main App

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import App from './App'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <QueryClientProvider client={queryClient}>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </QueryClientProvider>
 )
