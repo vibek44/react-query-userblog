aa8acb6d071cbd0ce320d6483be81fb905bec668
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-03-12 19:18:27 +0200

    importing BrowserRouter as Router and QueryClientProvider and passing it to main App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react'
 import { useQueryClient } from '@tanstack/react-query'
+import { Routes, Route } from 'react-router-dom'
+import Notification from './components/Notification'
+import MenuLink from './components/MenuLink'
+import Home from './components/Home'
+import LoginForm from './components/LoginForm'
+import BlogForm from './components/BlogForm'
+import Footer from './components/Footer'
 
 const App = () => {
   const queryClient = useQueryClient()
   return (
     <div>
-      <p>Welcome React</p>
+      <Notification />
+      <MenuLink />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/blogs" element={<BlogForm />} />
+      </Routes>
+      <Footer />
     </div>
   )
 }
