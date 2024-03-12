4bc8dc4b20577db6c8b5908b94a83eaec05fa44f
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-03-12 18:33:51 +0200

    adding hooks to reuse logic for form using component

---
+++ src/hooks/index.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react'
+
+export const useField = (type) => {
+  const [value, setValue] = useState('')
+
+  const onChange = (e) => {
+    setValue(e.target.value)
+  }
+
+  return {
+    type,
+    value,
+    onChange,
+  }
+}
