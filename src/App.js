158600aefd7736166c8d424378e02ec8bccbfc5e
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-03-14 19:14:05 +0200

    changing useBlogHook name to useLoginHook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import React from 'react'
-import { useQueryClient } from '@tanstack/react-query'
+import React, { useState } from 'react'
+import { useQuery, useQueryClient } from '@tanstack/react-query'
 import { Routes, Route } from 'react-router-dom'
 import Notification from './components/Notification'
 import MenuLink from './components/MenuLink'
 import Home from './components/Home'
 import LoginForm from './components/LoginForm'
-import BlogForm from './components/BlogForm'
+//import BlogForm from './components/BlogForm'
 import Footer from './components/Footer'
+import { useLoginHook } from './hooks'
 
 const App = () => {
-  const queryClient = useQueryClient()
+  const [credentials, setCredential] = useState(null)
+  const result = useLoginHook(credentials)
+
+  const handleLogin = (credential) => {
+    setCredential(credential)
+  }
+
   return (
     <div>
       <Notification />
+
       <MenuLink />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<LoginForm />} />
+        <Route
+          path="/login"
+          element={<LoginForm handleLogin={handleLogin} />}
+        />
+        {/*
         <Route path="/blogs" element={<BlogForm />} />
+  */}
       </Routes>
       <Footer />
     </div>
