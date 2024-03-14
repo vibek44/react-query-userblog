27a7beffb6c0c16a515be599848553d11c60e56f
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-03-14 19:13:52 +0200

    changing useBlogHook name to useLoginHook

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,4 +1,33 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState, useContext, useEffect } from 'react'
+import { UserContext, NotificationContext } from '../context'
+import { useQuery } from '@tanstack/react-query'
+import { verifyUser } from '../services/loginrequest'
+
+export const useLoginHook = (credentials) => {
+  const [, userDispatch] = useContext(UserContext)
+  const [, notificationDispatch] = useContext(NotificationContext)
+
+  useEffect(() => {
+    if (credentials) {
+      verifyUser(credentials)
+        .then((data) => {
+          userDispatch({ type: 'setUser', payload: data })
+        })
+        .catch((error) => {
+          notificationDispatch({
+            type: 'setNotification',
+            payload: error.response.data.error,
+          })
+          setTimeout(() => {
+            notificationDispatch({
+              type: 'setNotification',
+              payload: '',
+            })
+          }, 3000)
+        })
+    }
+  }, [credentials])
+}
 
 export const useField = (type) => {
   const [value, setValue] = useState('')
