7a7dda1ef618f985c29008b8590863c1ec63546a
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-03-16 15:00:07 +0200

    passing App component , Router,NotificationContextProvider, UserContextProvider

--- src/index.js
+++ src/index.js
@@ -3,13 +3,18 @@ import ReactDOM from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import App from './App'
 import { BrowserRouter as Router } from 'react-router-dom'
+import { NotificationContextProvider, UserContextProvider } from './context'
 
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <QueryClientProvider client={queryClient}>
     <Router>
-      <App />
+      <NotificationContextProvider>
+        <UserContextProvider>
+          <App />
+        </UserContextProvider>
+      </NotificationContextProvider>
     </Router>
   </QueryClientProvider>
 )
