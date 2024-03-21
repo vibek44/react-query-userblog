a7affda793898e4b13dde93e82432bd682ef214e
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-03-21 18:22:26 +0200

    adding inline style to Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-
+const Style = {
+  paddingTop: 10,
+}
 const Footer = () => {
   return (
-    <div>
-      Bloglist app for | <a href="http://localhost:3001">blog-user</a>. See{' '}
-      <a href="https://github.com/vibek44/react-query-userblog/blob/main/README.md">
+    <div style={Style}>
+      Bloglist app for | <a href='http://localhost:3001'>blog-user</a>. See{' '}
+      <a href='https://github.com/vibek44/react-query-userblog/blob/main/README.md'>
         userblogs
       </a>{' '}
       for the source code.
