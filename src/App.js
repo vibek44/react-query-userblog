b82bf3d917bbae65bc8535e73943642b6b97ee0f
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-03-21 17:36:12 +0200

    adding comment handling functionality, adding route for specific Blog or User display

--- src/App.js
+++ src/App.js
@@ -3,20 +3,23 @@ import { Navigate, useNavigate } from 'react-router-dom'
 import { Routes, Route } from 'react-router-dom'
 import Notification from './components/Notification'
 import MenuLink from './components/MenuLink'
-import Home from './components/Home'
 import LoginForm from './components/LoginForm'
 import BlogForm from './components/BlogForm'
 import Footer from './components/Footer'
 import Blogs from './components/Blogs'
+import Blog from './components/Blog'
+import UserBlog from './components/UserBlog'
 import { useBlogHook, useLoginHook } from './hooks'
 import { UserContext } from './context'
+import Users from './components/User'
 
 const App = () => {
   const navigate = useNavigate()
   const [user, userDispatch] = useContext(UserContext)
   const [credentials, setCredential] = useState(null)
   useLoginHook(credentials)
-  const { createMutation, likeMutation, removeMutation } = useBlogHook()
+  const { createMutation, likeMutation, removeMutation, commentMutation } =
+    useBlogHook()
 
   const handleLogin = (credential) => {
     setCredential(credential)
@@ -39,6 +42,10 @@ const App = () => {
     removeMutation.mutate(id)
   }
 
+  const handleComment = (id, comment) => {
+    commentMutation.mutate({ id, comment: { comment } })
+  }
+
   return (
     <div>
       <Notification />
@@ -46,26 +53,42 @@ const App = () => {
       <MenuLink handleLogOut={handleLogOut} />
       <Routes>
         <Route
-          path="/login"
+          path='/user/:id'
+          element={<UserBlog handleLogOut={handleLogOut} />}
+        />
+        <Route path='/users' element={<Users handleLogOut={handleLogOut} />} />
+        <Route
+          path='/login'
           element={<LoginForm handleLogin={handleLogin} />}
         />
         <Route
-          path="/blogs"
+          path='/blog/:id'
           element={
-            <Blogs handleLike={handleLike} handleRemove={handleRemove} />
+            <Blog
+              handleLogOut={handleLogOut}
+              handleLike={handleLike}
+              handleComment={handleComment}
+            />
           }
         />
+
         <Route
-          path="/create"
+          path='/create'
           element={
             user ? (
               <BlogForm createBlog={createBlog} />
             ) : (
-              <Navigate to="/login" />
+              <Navigate to='/login' />
             )
           }
         />
-        <Route path="/" element={<Home />} />
+
+        <Route
+          path='/'
+          element={
+            <Blogs handleLike={handleLike} handleRemove={handleRemove} />
+          }
+        />
       </Routes>
       <Footer />
     </div>
