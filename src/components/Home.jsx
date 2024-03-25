14c80eb324213f65404baa3daee76c127c1ca7d6
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-03-25 19:00:30 +0200

    Adding Home component

---
+++ src/components/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+const Home=()=>{
+  return(
+    <div>
+      <h2>User-Blog</h2>
+      <p>
+        User-Blog app is simply a site to check all the blogs, 
+        related to various field such as <em>Health,Technology,Histoy</em>etc.
+        In addition, users can check blogs created from different users specifically,
+        in one place &#128525; .
+        Users can also create own blogs, creating,liking,deleting is the basic functionality 
+        of the app 
+      </p>
+    </div>
+  )
+}
+
+export default Home
