371cbdf9bf45b1bd3eb5f734260510f7e64541f3
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-03-25 19:01:53 +0200

    Adding Navbar component fr0m react-bootstrap

--- src/components/MenuLink.jsx
+++ src/components/MenuLink.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import { UserContext } from '../context'
 import { Link, useNavigate } from 'react-router-dom'
+import { Navbar, Nav } from 'react-bootstrap'
 
 const MenuLink = ({ handleLogOut }) => {
   const navigate = useNavigate()
@@ -14,26 +15,40 @@ const MenuLink = ({ handleLogOut }) => {
   }
 
   return (
-    <div>
-      <Link style={Style} to='/'>
-        Blogs
-      </Link>
-
-      <Link style={Style} to='/users'>
-        users
-      </Link>
-
-      {user ? (
-        <>
-          {user.username} signed in
-          <button onClick={handleSignOut}>Logout</button>
-        </>
-      ) : (
-        <Link style={Style} to='/login'>
-          login
-        </Link>
-      )}
-    </div>
+    <Navbar collapseOnSelect expand='lg' bg='dark' variant='dark'>
+      <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+      <Navbar.Collapse id='responsive-navbar-nav'>
+        <Nav className='me-auto'>
+          <Nav.Link href='#' as='span'>
+            <Link style={Style} to='/home'>
+              Home
+            </Link>
+          </Nav.Link>
+          <Nav.Link href='#' as='span'>
+            <Link style={Style} to='/'>
+              Blogs
+            </Link>
+          </Nav.Link>
+          <Nav.Link href='#' as='span'>
+            <Link style={Style} to='/users'>
+              Users
+            </Link>
+          </Nav.Link>
+          <Nav.Link href='#' as='span'>
+            {user ? (
+              <>
+                {user.username} signed in
+                <button onClick={handleSignOut}>Logout</button>
+              </>
+            ) : (
+              <Link style={Style} to='/login'>
+                login
+              </Link>
+            )}
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   )
 }
 
